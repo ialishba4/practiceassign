61d8fafa5811608e3ac0a5ea3d7d6a6da9549cc6
ialishba4 <alishbahnoman326@gmail.com>
2023-12-08 19:36:17 +0500

    completed

---
+++ app.js
@@ -0,0 +1,111 @@
+// alert("Hello Javascript" );
+// console.log("hello javascript");
+// var firstPersonName = "zaid";
+// var marks = 76;
+// console.log(marks);
+
+// var marks = 86;
+// console.log("marks");
+// alert(marks);
+// alert(firstPersonName);
+// alert("Hello Javascript");
+// console.log("Hello Javascript");
+// var firstpersonName ="Zaid";
+// alert(firstpersonName);
+// var marks = 75;
+// alert(marks);
+// console.log(marks);
+
+// var a = 10;
+// var b = 20;
+// var c = 40;
+// var d = 60;
+
+// console.log( (a+b)-c*d);
+
+// var a = 20;
+// var b = a++;
+
+// console.log(b, "B value");
+// console.log(a, "A value");
+
+// var a = 10;
+// var b = 20;
+// var b = "20";
+// var c = a+b;
+
+// console.log(c);
+
+// var userName = prompt("Enter Your Name");
+// var age = prompt("Enter Your Age");
+// console.log (userName, age);
+
+
+// == === equal to
+// != !== not equal to
+// > greater than
+// < less than
+// <= less than equal to
+// >= greater than equal to
+// var age = 18;
+// var studentcard = true;
+// if(age >= 18){
+//     console.log("Condition True");
+// }else{
+//     if(studentcard == true){
+//         console.log("allow on student card");
+//     }
+//     else{
+//         console.log("Not Allow");
+// }
+//     }
+    // var age = 15;
+    // var studentCard = true;
+
+    // if(age >= 18){
+    //     console.log("Allow");
+    // }else if(studentCard == true) {
+    //     console.log("Allow On Student Card");
+    // } else{
+    //     console.log("Not Allow");
+    // }
+        
+    // var age = 10;
+    // var cnic = true;
+
+    // if(age >=18 && cnic == true){
+    //           console.log("Allow")
+    // }else{console.log("Not Allowed")}
+
+    //   var age = 10;
+    //   var cnic = true;
+    //   var studentCard = true;
+
+    //   if(age >=18 || studentCard == true){
+    //     console.log("Allow")}
+
+    var a = "A"
+    var b = "B"
+
+    var arr = ["A","B","C","D","E"]
+    // console.log(arr);
+
+    // arr[2]= 12;
+
+    // console.log(arr);
+
+     // arr[5]= "D";
+ 
+    // console.log(arr);
+
+    // arr.push("D");
+    // arr.pop();
+    // arr.shift();
+    // arr.unshift("1");
+    // console.log(arr);
+    // var a = arr.slice(1,4);
+    // console.log(a);
+
+   var a= arr.splice(2, 2 , "1", "2","3","4" );
+   console.log(a);
+     console.log(arr);
